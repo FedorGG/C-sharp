24d468be2ccfffedfb9bcdf40d4e799433b2f8d0
Fedor <fedorkolada@gmail.com>
2023-09-02 12:00:21 +0300

    Добавили проекты

---
+++ 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число N:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Целые числа в промежутке от -N до N:");
+        for (int i = -N; i <= N; i++)
+        {
+            Console.WriteLine(i);
+        }
---
+++ 5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        int lastValue = value % 10;
+      Console.WriteLine("Последняя цифра трёхзначного числа: " + lastValue);
