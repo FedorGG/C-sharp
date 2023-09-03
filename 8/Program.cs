c439addfebe52c70036ebb7f3f70ed4705ac7b8a
Fedor <fedorkolada@gmail.com>
2023-09-03 11:34:06 +0300

    добавил проект

---
+++ 8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        int Number = int.Parse(Console.ReadLine());
+        if (Number % 7 == 0 && Number % 23 == 0)
+        {
+            Console.WriteLine("Кратно");
+        }
+        else
+        {
+            int ost = Number % Number;
+            Console.WriteLine("Не кратно");
+        }
+    }
+}
