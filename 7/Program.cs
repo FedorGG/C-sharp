b9c012f0349ba54b5c6f198731d951e7ea8be8d4
Fedor <fedorkolada@gmail.com>
2023-09-03 11:18:30 +0300

    добавил проект

---
+++ 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        int fNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int sNumber = int.Parse(Console.ReadLine());
+        if (fNumber % sNumber == 0)
+        {
+            Console.WriteLine("Второе число кратно первому");
+        }
+        else
+        {
+            int ost = fNumber % sNumber;
+            Console.WriteLine("Второе число не кратно первому. Остаток от деления " + ost);
+        }
+    }
+}
